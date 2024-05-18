89086d1312e7bc2dee1473e4862ed353d65fbc5f
AL-AMIN <alamin50cse.miu@gmail.com>
2024-05-18 12:08:51 +0600

    linchart XAxis and YAxis add

--- src/components/Linechart/Linechart.jsx
+++ src/components/Linechart/Linechart.jsx
@@ -1,22 +1,25 @@
 
-import { LineChart, Line } from 'recharts';
+import { LineChart, Line, XAxis, YAxis } from 'recharts';
 const Linechart = () => {
     const studentMathMarksData = [
-        { id: 1, name: "Alice", math: 78 },
-        { id: 2, name: "Bob", math: 85 },
-        { id: 3, name: "Charlie", math: 92 },
-        { id: 4, name: "David", math: 74 },
-        { id: 5, name: "Eva", math: 88 },
-        { id: 6, name: "Frank", math: 95 },
-        { id: 7, name: "Grace", math: 67 },
-        { id: 8, name: "Hannah", math: 80 },
-        { id: 9, name: "Ivy", math: 83 },
-        { id: 10, name: "Jack", math: 90 }
+        { id: 1, name: "Alice", math: 78, physics: 82, chemistry: 85 },
+    { id: 2, name: "Bob", math: 85, physics: 89, chemistry: 90 },
+    { id: 3, name: "Charlie", math: 92, physics: 95, chemistry: 91 },
+    { id: 4, name: "David", math: 74, physics: 78, chemistry: 76 },
+    { id: 5, name: "Eva", math: 88, physics: 90, chemistry: 87 },
+    { id: 6, name: "Frank", math: 95, physics: 92, chemistry: 94 },
+    { id: 7, name: "Grace", math: 67, physics: 70, chemistry: 72 },
+    { id: 8, name: "Hannah", math: 80, physics: 85, chemistry: 83 },
+    { id: 9, name: "Ivy", math: 83, physics: 88, chemistry: 86 },
+    { id: 10, name: "Jack", math: 90, physics: 93, chemistry: 91 }
     ];
     return (
         <div>
-           <LineChart width={500} height={400} data={studentMathMarksData}>
-            <Line dataKey="math"></Line>
+           <LineChart width={700} height={300} data={studentMathMarksData}>
+            <XAxis dataKey="name"/>
+            <YAxis></YAxis>
+            <Line dataKey="math" stroke='red'></Line>
+            <Line dataKey="physics" stroke='yellow'></Line>
 
            </LineChart>
         </div>
